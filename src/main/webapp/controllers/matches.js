e9b7f7ca3d6d542d97c41d7b73493692fec4da84
ivannov <ivan_st_ivanov@yahoo.com>
2014-05-30 22:56:45 +0300

    Added a form for creating a new match. Introduced routing

--- src/main/webapp/controllers/matches.js
+++ src/main/webapp/controllers/matches.js
@@ -1,6 +1,41 @@
-angular.module('app', [])
-	.controller('MatchesCtrl', function($scope, $http) {
-		$http.get('rest/matches').success(function(data) {
-			$scope.matches = data.match;
-		});
-	});
+angular.module('app', [ 'ngRoute' ])
+	.controller('MatchesCtrl',
+		function($scope, $http, $location) {
+			$http.get('rest/matches').success(function(data) {
+				$scope.matches = data.match;
+			});
+
+			$http.get('rest/teams').success(function(data) {
+				$scope.teams = data.team;
+			});
+
+			$scope.createMatch = function() {
+				var match = {
+					"match" : {
+						"team1" : $scope.team1,
+						"team2" : $scope.team2,
+						"result" : {
+							"team1Score" : $scope.team1Score,
+							"team2Score" : $scope.team2Score
+						},
+						"matchDate" : $scope.matchDate
+					}
+				};
+				$http.post('rest/matches', match).success(function() {
+					$location.path('/matches');
+				});
+			};
+		})
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/matches', {
+				templateUrl: "views/allmatches.html",
+				controller: "MatchesCtrl"
+			})
+			.when('/matches/create', {
+				templateUrl: "views/creatematch.html",
+				controller: "MatchesCtrl"
+			})
+			.otherwise({
+				redirectTo: "/matches"
+			});
+	}]);
